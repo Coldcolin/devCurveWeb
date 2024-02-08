cfea0630f3ee2615a3f1e89473c8190b60f50aad
HP PROBOOK <decorcecolin@gmail.com>
2024-02-08 16:30:24 +0100

    new features added

--- src/Components/gallery/GalleryBody.jsx
+++ src/Components/gallery/GalleryBody.jsx
@@ -4,10 +4,14 @@ import GalleryItem from "./GalleryItem.jsx"
 const GalleryBody = () => {
   return (
     <div className="GalleryBody">
+        <div className='Hero1'>
+            <marquee>Registration for the The Curve Africa Academy cohort 4.0 is now open!</marquee>
+        </div>
       <h1>Step into Our World</h1>
       <p>
       Explore a collection of captivating photos showcasing the vibrant energy and exciting moments from our past events and activities. Immerse yourself in the visual journey that highlights the engaging experiences and the incredible community that makes our academy thrive. Witness the enthusiasm, collaboration, and passion that define our unique tech ecosystem.
       </p>
+      <h2>Event photos from our gallery</h2>
       <section>
         {
           Image.map((e)=>(
